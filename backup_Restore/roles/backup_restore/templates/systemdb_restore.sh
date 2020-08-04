bdce71f728bc340d7a081c790becc7317b8631ab
Mahesh Ramachandrappa <mramachandrappa@sst.local>
2020-08-04 14:58:33 +0530

    Added db_sid & db_sysnr vars

--- backup_Restore/roles/backup_restore/templates/systemdb_restore.sh
+++ backup_Restore/roles/backup_restore/templates/systemdb_restore.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 export bkp="{{ lookup('ini', 'systemdb_backup_path section=SAP file={{ config_file_path }}') }}"
-. /hana/shared/SS2/HDB00/hdbenv.sh
+. /hana/shared/{{ db_sid }}/HDB{{ db_sysnr }}/hdbenv.sh
 HDBSettings.sh recoverSys.py --command="RECOVER DATA USING FILE ('$bkp') CLEAR LOG" --wait --timeout=600
