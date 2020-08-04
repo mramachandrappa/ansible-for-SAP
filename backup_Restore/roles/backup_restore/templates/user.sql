250cad213daa3171d75f05631024ed2844b40293
Mahesh Ramachandrappa <mramachandrappa@sst.local>
2020-08-04 20:15:32 +0530

    Updated scripts

--- backup_Restore/roles/backup_restore/templates/user.sql
+++ backup_Restore/roles/backup_restore/templates/user.sql
@@ -1,3 +1,5 @@
 create user bkp_operator password Initial1;
 grant backup operator to bkp_operator;
+grant backup admin to bkp_operator;
+grant catalog read to bkp_operator;
 alter user bkp_operator disable password lifetime;
