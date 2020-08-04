1d4650c6ccb0cc6fad9631082b119c8e65e6f81b
Mahesh Ramachandrappa <mramachandrappa@sst.local>
2020-08-04 19:40:45 +0530

    Updated scripts

--- backup_Restore/roles/backup_restore/templates/user_sql.sh
+++ backup_Restore/roles/backup_restore/templates/user_sql.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 . /hana/shared/{{ db_sid }}/HDB{{ db_sysnr }}/hdbenv.sh
-hdbsql -i {{ db_sysnr }} -d SYSTEMDB -u SYSTEM -p {{ source_db_system_user_pass }} -I /tmp/user.sql
-hdbuserstore -i SET backup {{ db_hostname }}:3{{ db_sysnr }}15 bkp_operator Initial1
+hdbsql -i {{ db_sysnr }} -d SYSTEMDB -u SYSTEM -p {{ source_db_systemuser_passwd }} -I /tmp/user.sql
+hdbuserstore -i SET backup {{ db_hostname }}:3{{ db_sysnr }}13 BKP_OPERATOR Initial1
