f1787fc08a28cc02963a610678ccfe5ee38d9285
Mahesh Ramachandrappa <mramachandrappa@sst.local>
2020-08-04 19:07:08 +0530

    Updated scripts

--- backup_Restore/roles/backup_restore/templates/schema_change.sh
+++ backup_Restore/roles/backup_restore/templates/schema_change.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-hdbuserstore SET DEFAULT {{ db_hostname }}:3{{ db_sysnr ))15 SAPABAP1 {{ source_schema_passwd }}
+. /hana/shared/{{ db_sid }}/HDB{{ db_sysnr }}/hdbenv.sh
+hdbuserstore SET DEFAULT {{ db_hostname }}:3{{ db_sysnr }}15 SAPABAP1 {{ source_schema_passwd }}
--- backup_Restore/roles/backup_restore/templates/user_sql.sh
+++ backup_Restore/roles/backup_restore/templates/user_sql.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+. /hana/shared/{{ db_sid }}/HDB{{ db_sysnr }}/hdbenv.sh
 hdbsql -i {{ db_sysnr }} -d SYSTEMDB -u SYSTEM -p {{ source_db_system_user_pass }} -I /tmp/user.sql
 hdbuserstore -i SET backup {{ db_hostname }}:3{{ db_sysnr }}15 bkp_operator Initial1
