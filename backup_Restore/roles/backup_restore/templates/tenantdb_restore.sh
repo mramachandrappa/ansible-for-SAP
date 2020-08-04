81dd229f6c31f2aff6c86074251f816c3077db08
Mahesh Ramachandrappa <mramachandrappa@sst.local>
2020-08-04 19:14:41 +0530

    Updated scripts

--- backup_Restore/roles/backup_restore/templates/tenantdb_restore.sh
+++ backup_Restore/roles/backup_restore/templates/tenantdb_restore.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 export bkpi="{{ lookup('ini', 'tenantdb_backup_path section=SAP file={{ config_file_path }} ') }}"
 . /hana/shared/{{ db_sid }}/HDB{{ db_sysnr }}/hdbenv.sh
-hdbsql -U BACKUPSYS "RECOVER DATA USING FILE ('$bkpi') CLEAR LOG" --wait --timeout=600
+hdbsql -U BACKUP "RECOVER DATA USING FILE ('$bkpi') CLEAR LOG" --wait --timeout=600
