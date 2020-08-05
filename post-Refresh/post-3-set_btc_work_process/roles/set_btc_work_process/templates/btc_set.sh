c2507842a987e2fe5e74e743b9bd456d9cfb56b2
Mahesh Ramachandrappa <mramachandrappa@sst.local>
2020-08-05 18:04:38 +0530

    Updated role

--- post-Refresh/post-3-set_btc_work_process/roles/set_btc_work_process/templates/btc_set.sh
+++ post-Refresh/post-3-set_btc_work_process/roles/set_btc_work_process/templates/btc_set.sh
@@ -3,3 +3,4 @@ stopsap r3
 sleep 10
 mv /usr/sap/{{ target_sap_sid }}/SYS/profile/{{ target_sap_sid }}_DVEBMGS{{ sysnr }}_{{ ashost }}.bak /usr/sap/{{ target_sap_sid }}/SYS/profile/{{ target_sap_sid }}_DVEBMGS{{ sysnr }}_{{ ashost }}
 startsap r3
+sleep 120
